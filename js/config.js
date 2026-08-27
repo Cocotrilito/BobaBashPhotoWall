e84e7f4a9ca3fd3920404ae8b694d925f43e130a
Cocotrilo <nogalesisaac25@gmail.com>
2026-08-26 19:56:32 -0500

    Added Setup and started with config, upload and array for loop

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = ;
+const SUPABASE_KEY = "sb_publishable_ORdlmvP6xBnYZu5pYUsF4w_Tzx8OscP";
---
+++ js/upload.js
@@ -0,0 +1,3 @@
+const photoInput = document.getElementById("photoInput");
+const btnUpload = document.getElementById("btnUpload");
+const status= document.getElementById("status");
