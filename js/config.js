5fc7a93921df88e66e7940825b452200e280b714
Cocotrilo <nogalesisaac25@gmail.com>
2026-08-27 11:49:22 -0500

    Integration and upload works with the database and storage in supabasegit add .!

--- js/config.js
+++ js/config.js
@@ -1,2 +1,4 @@
-const SUPABASE_URL = ;
+const SUPABASE_URL = "https://qzlyzxzhjxbhlxvlbwdz.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ORdlmvP6xBnYZu5pYUsF4w_Tzx8OscP";
+
+const client = supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
--- js/upload.js
+++ js/upload.js
@@ -1,3 +1,33 @@
 const photoInput = document.getElementById("photoInput");
 const btnUpload = document.getElementById("btnUpload");
-const status= document.getElementById("status");
+const UpStatus= document.getElementById("UpStatus");
+
+
+
+btnUpload.addEventListener("click", async function() {
+    btnUpload.disabled = true;
+    btnUpload.classList.add("opacity-50");
+    for (const file of photoInput.files) {
+        const Filename = Date.now() + "-" + file.name;
+        const { data, error } = await client.storage.from("photos").upload(Filename, file);
+        if (error) {
+            console.error(error);
+            continue;
+        }; 
+        const { data: urlData } = client.storage.from("photos").getPublicUrl(Filename);
+
+        const { error: dbError } = await client.from("photowall").insert([
+            {photo_url: urlData.publicUrl, filename: file.name}
+        ]);
+
+        if (dbError) {
+            console.error(dbError);
+            UpStatus.textContent = "Error storaged in database"
+            continue;
+        }
+
+        UpStatus.textContent = "Successful Upload!";
+    }
+    btnUpload.disabled = false;
+    btnUpload.classList.remove("opacity-50");
+});
